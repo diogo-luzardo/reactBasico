037bc61c225b0f6ca952cfe491dbed57940691eb
diogo-luzardo <54155950+diogo-luzardo@users.noreply.github.com>
2019-09-19 14:41:23 -0300

    Adicionando um foto para usuario

--- projeto2/src/Diogo.js
+++ projeto2/src/Diogo.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import User from './User'
+import photo from './img/user.png'
+
+
 
 class Diogo extends Component {
     render(){
         return(
             <div>
                 Eu sou um component chamado Diogo.
+                <User name="Diogo" photo={photo}/>
             </div>
         )
     }
