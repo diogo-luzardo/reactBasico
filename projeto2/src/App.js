bf9c420a31f5700af95039a8168d18fb3ffdcba5
diogo-luzardo <54155950+diogo-luzardo@users.noreply.github.com>
2019-09-19 15:45:36 -0300

    Adicionado botão que modifica o state da aplicação

--- projeto2/src/App.js
+++ projeto2/src/App.js
@@ -1,21 +1,51 @@
-import React from 'react';
+import React, { Component }from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Diogo from './Diogo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Meu projeto em React
-        </p>
-      <Diogo></Diogo>
-      </header>
-      
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      name:"Arthur"
+    }
+    this.changeState = this.changeState.bind(this)
+    this.resetState = this.resetState.bind(this)
+  }
+
+  changeState() {
+    this.setState({
+      name: "Diogo Faria"
+    })
+  }
+
+  resetState() {
+    this.setState({
+      name: "Arthur"
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Meu projeto em React
+          </p>
+            <Diogo />
+          <div>
+            {this.state.name}
+          </div>
+          <div>
+            <button onClick={this.changeState}>Mudar nome</button>
+            <button onClick={this.resetState}>Resetar o nome</button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
